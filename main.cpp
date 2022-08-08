d76e741c2710503a83308f8758a7f131bbdeb28a
Grisha <75669384+peon12345@users.noreply.github.com>
2022-08-08 20:21:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 
 int main(int argc, char *argv[])
 {
-
-  std::tuple<char,std::pair<char,char> , std::tuple<char,char,char>> num;
-  QString numStr = QString::number(sizeof(std::get<0>(num))) + QString::number(sizeof(std::get<1>(num))) + QString::number(sizeof(std::get<2>(num)));
-
   std::vector<uint> nums = {1,2,123,4,5,6,7,9,10,12,13,11,11,23,11,11,245,246,247,248};
   std::vector<uint>::iterator it = nums.begin();
   std::vector<uint>::iterator oldIt = nums.begin();
